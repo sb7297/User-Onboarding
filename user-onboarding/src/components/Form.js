3a4b2850f1d606cbd5de8bea78742a4a256ea1c3
sb7297 <sbaireddy7297@gmail.com>
2021-07-21 13:08:44 -0400

    Form: add validation & error display to input data

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as Yup from "yup";
 
 const Form = function() {
   const [form, setForm] = useState({
@@ -8,14 +9,60 @@ const Form = function() {
     tosCheck: false,
   });
 
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    password: '',
+    tosCheck: '',
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  const formSchema = Yup.object().shape({
+    name: Yup
+      .string()
+      .required("User name is required"),
+    email: Yup
+      .string()
+      .email("You must enter a valid email")
+      .required("User email is required"),
+    password: Yup
+      .string()
+      .required("Password is required")
+      .min(6, "Passwords must be at least 6 characters"),
+    tosCheck: Yup
+      .boolean()
+      .oneOf([true], "You must accept the Terms of Service")
+  });
+
   const handleChange = ev => {
+    const { name } = ev.target;
+    const data = ev.target.type === "checkbox" ? ev.target.checked : ev.target.value;
+
+    Yup
+      .reach(formSchema, name)
+      .validate(data)
+      .then(valid => {
+        setErrors({ ...errors, [name]: '' });
+      })
+      .catch(err => {
+        setErrors({ ...errors, [name]: err.errors[0] })
+      });
+
     setForm({
       ...form,
-      [ev.target.name]: ev.target.type === "checkbox" ? ev.target.checked : ev.target.value
+      [name]: data
     });
   };
 
+  useEffect(() => {
+    formSchema.isValid(form).then(valid => {
+      setButtonDisabled(!valid);
+    });
+  }, [form]);
+
   return (
+    <div className="Form">
     <form>
       <label>User name:
         <input value={form.name} name="name" type="text" onChange={handleChange} />
@@ -29,8 +76,15 @@ const Form = function() {
       <label>Accept TOS?
         <input checked={form.tosCheck} name="tosCheck" type="checkbox" onChange={handleChange} />
       </label>
-      <button>Submit</button>
+      <button disabled={buttonDisabled}>Submit</button>
     </form>
+    <div className="errors" style={{color: 'maroon'}}>
+      {errors.name.length > 0 && <p>{errors.name}</p>}
+      {errors.email.length > 0 && <p>{errors.email}</p>}
+      {errors.password.length > 0 && <p>{errors.password}</p>}
+      {errors.tosCheck.length > 0 && <p>{errors.tosCheck}</p>}
+    </div>
+    </div>
   );
 }
 

7f3ff31a4308d8621657bacbe5a5840649898519
sb7297 <sbaireddy7297@gmail.com>
2021-07-22 18:42:38 -0400

    test form validation

--- user-onboarding/cypress/integration/form.spec.js
+++ user-onboarding/cypress/integration/form.spec.js
@@ -43,3 +43,48 @@ describe('Form submission', function() {
       .should('exist');
   })
 })
+
+describe('Empty submission', function() {
+  it("can't submit form if inputs are empty", function() {
+    cy.visit("index.html");
+    
+    cy.get('[data-cy=name]')
+      .should("have.value" , "");
+
+    cy.get('[data-cy=email]')
+      .should("have.value" , "");
+
+    cy.get('[data-cy=password]')
+      .should("have.value" , "");
+
+    cy.get('[data-cy=tosCheck]')
+      .should("not.be.checked");
+
+    cy.get('[data-cy=submit]')
+      .should("be.disabled");
+  })
+})
+
+describe('Invalid email', function() {
+  it("can't submit form if email is invalid", function() {
+    cy.visit("index.html");
+    
+    cy.get('[data-cy=name]')
+      .type('Naruto Uzumaki');
+
+    cy.get('[data-cy=email]')
+      .type('naruto');
+
+    cy.get('[data-cy=password]')
+      .type("jajpgajwporgjpoajg");
+
+    cy.get('[data-cy=tosCheck]')
+      .click();
+
+    cy.get('[data-cy=submit]')
+      .should("be.disabled");
+
+    cy.get(".errors")
+      .contains("valid email");
+  })
+})

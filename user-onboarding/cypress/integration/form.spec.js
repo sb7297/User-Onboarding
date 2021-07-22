ba3ea6b72e314ef7b1dc2ba76f2ec975155c6568
sb7297 <sbaireddy7297@gmail.com>
2021-07-22 12:56:53 -0400

    test tos checkbox entry

--- user-onboarding/cypress/integration/form.spec.js
+++ user-onboarding/cypress/integration/form.spec.js
@@ -7,4 +7,22 @@ describe('Name entry', function () {
       .type("Naruto Uzumaki")
       .should("have.value", "Naruto Uzumaki");
   })
+});
+
+describe('TOS Check', function() {
+  it('can be checked', function() {
+    cy.visit("index.html");
+    cy.get('[data-cy=name]')
+      .type("Naruto Uzumaki");
+
+    cy.get('[data-cy=email]')
+      .type("hokage@konoha.gov");
+
+    cy.get('[data-cy=password]')
+      .type("ajopjkapkaph");
+
+    cy.get('[data-cy=tosCheck]')
+      .click()
+      .should('be.checked');
+  })
 })
